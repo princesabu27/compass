a9bc3d63a426478d16148b93bc15807d233cfc37
Prince Sabu <PSabu@merpsystems.com>
2025-05-26 19:56:02 +0530

    style the composs logo

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "/compass/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    assetsDir: "assets",
+  },
 });
